b0db49698caf93ed3969d9cb3042e0a03f334390
Aid3n_Pearce <61540835+Aid-3n@users.noreply.github.com>
2023-05-21 16:26:25 +0530

    VSC Is lub

--- cart.py
+++ cart.py
@@ -1,16 +1,31 @@
 product_list = { "Item A": 20, "Item B": 40, "Item C": 50 } #Items_List as per question given
-
-
-discounts = {
-    "flat10": 10 if subtotal > 200 else 0, #Discount option 1
-    "bulk5": sum(products[product] * quantities[product] * 0.05 if quantities[product] > 10 else 0 for product in products), #Discount option 2
-    "bulk10": subtotal * 0.1 if sum(quantities.values()) > 20 else 0, #Discount option 3
-    "dis50": sum(products[product] * max(0, quantities[product] - 15) * 0.5 if quantities[product] > 15 else 0 for product in products) #Discount option 4 }
-  
 quantities = {}
 giftwrap = {}
 for product in product_list:
     quantities[product] = int(input(f"Enter the number of {product}: "))
-    gift_wrap[product] = input(f"Should {product} need gift wrap? (y/n): ") == "y"
-  
-  
+    giftwrap[product] = input(f"Should {product} need gift wrap? (y/n): ") == "y"
+
+ship_fee = 5 * (sum(quantities.values()) // 10 + (1 if sum(quantities.values()) % 10 > 0 else 0)) 
+gift_fee = sum(quantities[product] if giftwrap[product] else 0 for product in product_list) 
+subtot = sum(product_list[product] * quantities[product] for product in product_list) #Calculaing subtoal
+
+discounts = {
+    "flat10": 10 if subtot > 200 else 0, 
+    "bulk5": sum(product_list[product] * quantities[product] * 0.05 if quantities[product] > 10 else 0 for product in product_list), 
+    "bulk10": subtot * 0.1 if sum(quantities.values()) > 20 else 0, 
+    "dis50": sum(product_list[product] * max(0, quantities[product] - 15) * 0.5 if quantities[product] > 15 else 0 for product in product_list) }
+
+discountt = max(discounts, key=discounts.get) 
+dis_amt = discounts[discountt]
+subto_with_disc = subtot - dis_amt
+
+total = subto_with_disc + ship_fee + gift_fee #Total bill with disco
+
+print("\nDetails of Product chosen")
+for product in product_list:
+    print(f"{product}: {quantities[product]} units, total amount: ${product_list[product] * quantities[product]}")
+print(f"Subtotal: ${subtot}")
+print(f"Discount applied ({discountt}): -${dis_amt}")
+print(f"Shipping fee: ${ship_fee}")
+print(f"Gift wrap fee: ${gift_fee}")
+print(f"Total Amount: ${total}")
